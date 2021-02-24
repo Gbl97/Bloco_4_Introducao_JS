ecac88d85b5d25bf8852bacdabd1f65e5be890ab
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-02-24 16:14:54 -0300

    Exercicio 2 - Feito

---
+++ dia_1/exercises_2/script.js
@@ -0,0 +1,31 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+patientId = '50';
+console.log(typeof patientId);
+
+let base;
+let altura;
+
+base = 5;
+altura = 8;
+
+let area;
+
+area = (base * altura);
+
+console.log(area);
+
+let perimetro;
+perimetro = (2*(base+altura));
+console.log(perimetro);
