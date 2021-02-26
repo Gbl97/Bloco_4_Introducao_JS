0ecbfedf63711527b3946b63a76baa57c896cb54
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-02-26 19:08:40 -0300

    Exercicio 2 - Feito

---
+++ dia_3/exercicios/exercises_2.js
@@ -0,0 +1,18 @@
+let n;
+
+
+n = 5;
+
+for (let n = 5; n > 1; n-=1) {
+    
+    if(n === 5){
+        console.log("* ");
+    } else if (n === 4) {
+        console.log("** ");
+    } else if (n === 3) {
+        console.log("*** ");
+    } else if (n === 2) {
+        console.log("**** ");
+        console.log("*****");
+    } 
+}
