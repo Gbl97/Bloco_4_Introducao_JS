35421ae0fe1f7a3ddb8e649fb1747080adda844c
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-01 16:27:26 -0300

    Adicionando o Exemplo 1

---
+++ dia_4/exemplos/objetos.js
@@ -0,0 +1,9 @@
+let player = {
+    nome: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
+};
+
+console.log("A jogadora " + player.nome + ' ' + player.lastName + " tem " + player.age + " anos de idade");
+
