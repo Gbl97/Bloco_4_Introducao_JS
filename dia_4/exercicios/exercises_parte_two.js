981dd719f8524d5b8e6707012d660f7472b9fd70
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-02 11:45:32 -0300

    Exercicio 4 - Criado

--- dia_4/exercicios/exercises_parte_two.js
+++ dia_4/exercicios/exercises_parte_two.js
@@ -51,3 +51,4 @@ function menorIndice(array_test) {
     return console.log(menor);
 }
 
+// Exercicio 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
