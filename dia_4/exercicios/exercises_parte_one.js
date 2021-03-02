a9bcaf5780089bec45cf50cd9d465103b1172f16
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-02 09:41:30 -0300

    Exercicio 1 - Feito

---
+++ dia_4/exercicios/exercises_parte_one.js
@@ -0,0 +1,10 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  // 1 - Imprimir uma mensagem de boas-vindas:
+  console.log("Bem-vinda, " + info.personagem);
+
+  // 2 - 
