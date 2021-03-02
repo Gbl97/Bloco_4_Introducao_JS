c6987507713f7709c83d8e5ac0069f6a66cc6108
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-02 11:44:00 -0300

    Exercicio 3 - Feito

--- dia_4/exercicios/exercises_parte_two.js
+++ dia_4/exercicios/exercises_parte_two.js
@@ -4,32 +4,50 @@
 // .join('') = uni todos os campos em uma string novamente;
 // referência: https://www.devmedia.com.br/forum/inverter-string-no-javascript/570951
 
-function verificaPalindrome(nome) {
-    let nome_inverso = nome.split('').reverse().join('');
+// function verificaPalindrome(nome) {
+//     let nome_inverso = nome.split('').reverse().join('');
     
-    if (nome === nome_inverso) {
-        return console.log(true);
-    } else {
-        return console.log(false);
-    }
-}
+//     if (nome === nome_inverso) {
+//         return console.log(true);
+//     } else {
+//         return console.log(false);
+//     }
+// }
 
-verificaPalindrome('mirim');
+// verificaPalindrome('mirim');
 
 // Exercicio 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
-let array_teste = [2, 3, 6, 7, 10, 1];
+// let array_teste = [2, 3, 6, 7, 10, 1];
 
 
-function maiorIndice(array_test) {
-    let maior = 0;
+// function maiorIndice(array_test) {
+//     let maior = 0;
+//     let index;
+
+//     for (index = 0; index < array_test.length; index+=1) {
+//         if (array_test[index] > maior) {
+//             maior = index;
+//         }
+//     }
+//     return console.log(maior);
+// }
+
+// maiorIndice(array_teste);
+
+// Exercício 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+let array_teste_2 = [2, 4, 6, 7, 10, 0, -3];
+
+menorIndice(array_teste_2);
+
+function menorIndice(array_test) {
+    let menor = 10000000;
     let index;
 
     for (index = 0; index < array_test.length; index+=1) {
-        if (array_test[index] > maior) {
-            maior = index;
+        if (array_test[index] < menor) {
+            menor = index;
         }
     }
-    return console.log(maior);
+    return console.log(menor);
 }
 
-maiorIndice(array_teste);
