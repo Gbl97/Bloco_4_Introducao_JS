a2b8cb6491b7843032f32597d56a93982913c9bd
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-03 10:16:02 -0300

    Requisito 5 - Feito

--- dia_4/exercicios/exercises_parte_two.js
+++ dia_4/exercicios/exercises_parte_two.js
@@ -35,21 +35,21 @@
 // maiorIndice(array_teste);
 
 // Exercício 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
-let array_teste_2 = [2, 4, 6, 7, 10, 0, -3];
+// let array_teste_2 = [2, 4, 6, 7, 10, 0, -3];
 
-menorIndice(array_teste_2);
+// menorIndice(array_teste_2);
 
-function menorIndice(array_test) {
-    let menor = 10000000;
-    let index;
+// function menorIndice(array_test) {
+//     let menor = 10000000;
+//     let index;
 
-    for (index = 0; index < array_test.length; index+=1) {
-        if (array_test[index] < menor) {
-            menor = index;
-        }
-    }
-    return console.log(menor);
-}
+//     for (index = 0; index < array_test.length; index+=1) {
+//         if (array_test[index] < menor) {
+//             menor = index;
+//         }
+//     }
+//     return console.log(menor);
+// }
 
 // Exercicio 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 // let arrayNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
@@ -71,3 +71,30 @@ function menorIndice(array_test) {
 // }
 
 //Exercicio 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+let arrayInteiro = [2, 3, 2, 5, 8, 2, 3];
+
+inteiroQueMaisSeRepete(arrayInteiro);
+
+function inteiroQueMaisSeRepete(arrayNumbers) {
+    // Cria uma variável que recebe a contagem dos números repetidos, e inicialmente, atribui-se a variável a 0:
+    let contadorRepetido = 0;
+    // Cria uma variável que recebe a contagem dos números, atribui-se a variável a 0:
+    let contadorNumbers = 0;
+    // Crio uma variável que nela é atribuída o index do número repetido:
+    let indexDoNumeroQueRepete = 0;
+
+    for (let index = 0; index < arrayNumbers.length; index+=1) {
+        for (let index_2 = 0; index_2 < arrayNumbers.length; index_2+=1) {
+            if (arrayNumbers[index] === arrayNumbers[index_2]) {
+                contadorNumbers+=1;
+            }         
+        }
+        if (contadorNumbers > contadorRepetido) {
+            contadorRepetido = contadorNumbers;
+            indexDoNumeroQueRepete = index;
+        }
+        contadorNumbers = 0;
+    }
+    return console.log("Número que mais se repete: " + arrayNumbers[indexDoNumeroQueRepete]+ " " + " Quanta vezes se repetiu o número: " + contadorRepetido);
+
+}
